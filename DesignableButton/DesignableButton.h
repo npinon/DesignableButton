81605b9fa48c5a240875d83a99b950093cdcf047
Nicolas <pinon.nico@gmail.com>
2018-02-02 13:14:44 +0100

    Initial Commit

---
+++ DesignableButton/DesignableButton.h
@@ -0,0 +1,19 @@
+//
+//  DesignableButton.h
+//  DesignableButton
+//
+//  Created by Nicolas Pinon on 02/02/2018.
+//  Copyright © 2018 Nicolas Pinon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesignableButton.
+FOUNDATION_EXPORT double DesignableButtonVersionNumber;
+
+//! Project version string for DesignableButton.
+FOUNDATION_EXPORT const unsigned char DesignableButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignableButton/PublicHeader.h>
+
+
